ca5fd63b7b8e18ec9e38f52e02afa312f15f976a
ctle-vn <ctle@sfu.ca>
2018-01-13 20:59:41 -0800

    Edited License smart contract and prepared interface web3 file

--- contracts/License.sol
+++ contracts/License.sol
@@ -1,18 +1,45 @@
 pragma solidity ^0.4.11;
 
 contract License {
-   address user;
-   uint timestamp;
+/***/
 
-   // Creates the contract
-   function License(){
-      user = msg.sender;
-      timestamp = now; // Timestamp of when contract is made
-   }
+      // This is a mapping that works like a dictionary or associated array in other languages.
+      mapping (address => bytes32[]) licenses;
 
-   // Destroy contract
-   function kill(){
-      if(msg.sender == user)
-         selfdestruct(userId);
-   }
+      // This registers an event
+      event Transfer(
+                  address indexed _from,
+                  address indexed _to,
+                  bytes32 license,
+                  string timeIssued,
+                  string timeExpiration,
+                  uint amount
+      );
+
+      // The contract constructor, which is called when the contract is deployed to the blockchain. 
+      // The contract is persistent on the blockchain, so it remains until it is removed.
+      function License( bytes32[] aLicenses ) {
+            if (aLicenses.length != 0) {
+                  licenses[tx.origin] = aLicenses;
+            }
+        }
+
+      // This method modifies the blockchain. The sender is required to fuel the transaction in Ether.
+      // only here, code to modify the chain
+      /**
+      *  receiver - address of the receiver who is receiving the license key
+      *  license - key of the license
+      *  timeIssued - issue time of the license in YYYYMMDD
+      *  timeExpiration - expieration time of the license in YYYYMMDD
+      *  amount - number of license keys 
+       */
+      function issueLicense(address receiver, bytes32 license, string timeIssued, string timeExpiration, uint amount) returns(bool sufficient) {
+            licenses[receiver].push(license);
+            Transfer(msg.sender, receiver, license, timeIssued, timeExpiration, amount); //opens the listener for the event, triggers the event
+            return true;
+      }
+      // This method does not modify the blockchain, so it does not require an account to fuel for the call.
+      function getLinceses(address addr) returns(bytes32[]) {
+            return licenses[addr];
+      }
 }
