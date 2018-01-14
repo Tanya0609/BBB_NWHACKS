ad6ca3507ad12ec844cd8938d843df761d14aecf
ctle-vn <ctle@sfu.ca>
2018-01-13 23:33:47 -0800

    Address array memory alloc issue

--- contracts/CompanyLicense.sol
+++ contracts/CompanyLicense.sol
@@ -3,18 +3,17 @@ pragma solidity ^0.4.11;
 contract CompanyLicense {
 
       struct License {
-            bytes32 licenseKey;
+            uint256 licenseKey;
             uint256 timeIssued;
             uint256 timeExpiration;
       }
       // This is a mapping that works like a dictionary or associated array in other languages.
-      mapping (address => License[]) licenses;
-
+      //mapping (address => License[]) licenses;
+        mapping (address => uint256) licenses;
       // This registers an event
       event Transfer(
             address indexed _from,
             address indexed _to,
-            License[] license,
             uint256 timeIssued,
             uint256 timeExpiration,
             uint amount
@@ -22,13 +21,15 @@ contract CompanyLicense {
 
       // The contract constructor, which is called when the contract is deployed to the blockchain. 
       // The contract is persistent on the blockchain, so it remains until it is removed.
-      function CompanyLicense( bytes32[] aLicenseKeys ) {
-            if (aLicenseKeys.length != 0) {
-                  for (uint i= 0; i < aLicenseKeys.length; i++) {
-                        licenses[tx.origin][i].licenseKey = aLicenseKeys[i];
-                  }
-            }
-        }
+      function CompanyLicense() { // uint256[] aLicenseKeys 
+            //licenses[tx.origin] = new License[](aLicenseKeys.length);
+            //if (aLicenseKeys.length > 0 && licenses[tx.origin].length > 0) {
+            //      for (uint i= 0; i < aLicenseKeys.length; i++) {
+            //            licenses[tx.origin][i].licenseKey = aLicenseKeys[i];
+            //      }
+            licenses[0xd85f9E0E8906b8f120241bF50B0B7c4DdFaeCf16] = 24243;
+        
+      }
 
       // This method modifies the blockchain. The sender is required to fuel the transaction in Ether.
       // only here, code to modify the chain
@@ -38,7 +39,9 @@ contract CompanyLicense {
       *  timeIssued - issue time of the license in YYYYMMDD
       *  timeExpiration - expieration time of the license in YYYYMMDD
       *  amount - number of license keys 
-       */
+      * 
+      * 
+      * 
       function issueLicenses(address receiver, uint amount, uint256 timeIssued, uint256 timeExpiration) returns(bool sufficient) {
             if (amount > 0 && licenses[msg.sender].length >= amount) {
                   License oLicenseToUser;
@@ -49,13 +52,16 @@ contract CompanyLicense {
                         licenses[receiver].push(oLicenseToUser);
                   }
                   
-                  Transfer(msg.sender, receiver, licenses[receiver], timeIssued, timeExpiration, amount); //opens the listener for the event, triggers the event
+                  Transfer(msg.sender, receiver, timeIssued, timeExpiration, amount); //opens the listener for the event, triggers the event
                   return true;
-            }
+            } 
       }
+       */
+    
 
       // This method does not modify the blockchain, so it does not require an account to fuel for the call.
-      function getLicenses(address addr) returns(License[]) {
+      function getLicenses(address addr) returns(uint256) {
+    //function getLicenses(address addr) returns(License[]) {
             return licenses[addr];
       }
 /*
